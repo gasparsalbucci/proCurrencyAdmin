2226942de3fd0bfd011b07eb198f3b399bb4994c
Gaspar Salbucci <gasparsalbucci@gmail.com>
2019-04-15 19:28:02 -0300

    Gaspar Salbucci - First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './shared';
+
+const routes: Routes = [
+    { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
+    { path: 'login', loadChildren: './modules-view/login/login.module#LoginModule' },
+    { path: 'signup', loadChildren: './modules-view/signup/signup.module#SignupModule' },
+    { path: '**', redirectTo: 'login' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
