d22ea65d093a93dd2b7b89a4258c3b154e5c9bb8
Gaspar Salbucci <gasparsalbucci@gmail.com>
2019-04-15 20:14:49 -0300

    Gaspar Salbucci - Add http Interceptor class

---
+++ src/app/shared/services/http-interceptor.ts
@@ -0,0 +1,92 @@
+﻿import { Injectable, Injector } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { tap } from 'rxjs/operators';
+import { TaskManagerService } from "./task-manager.service";
+// import { environment } from "../../environments/environment";
+import { Router } from "@angular/router";
+
+@Injectable()
+export class HttpMessageInterceptor implements HttpInterceptor {
+    router;
+    constructor(private taskManager: TaskManagerService, private injector: Injector) {
+        // Fix for ciclyc dependency
+        setTimeout(() => {
+            this.router = injector.get(Router);
+        });
+
+    }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        let taskId = 'generic';
+        if (!req.headers.get('task_name')) {
+            console.log("PLEASE ADD A TASK NAME FOR THE REQUEST");
+            req.headers.append("task_name", 'generic');
+        } else {
+            taskId = req.headers.get('task_name');
+        }
+        let Headers = req.headers.append("Content-Type", 'application/json').append("Accept", 'application/json');
+        if(localStorage.getItem('tk')) {
+            Headers = new HttpHeaders({ 'x-authorization': 'Bearer ' + localStorage.getItem('tk') });
+        }
+        
+        const changedReq = req.clone({ headers: Headers });
+        const currentTask = { id: taskId, status: 1, loading: true };
+        this.taskManager.addTask(currentTask);
+
+
+        return next.handle(changedReq).pipe(
+            tap(
+                (event: HttpEvent<any>) => {
+                    if (event instanceof HttpResponse) {
+                        this._handleSuccessResponse(event, taskId);
+                    }
+                }, 
+                (err: any) => {
+                    if (err instanceof HttpErrorResponse) {
+                        this._handleErrorResponse(err, taskId);
+                    }
+                }
+            )
+        )
+    }
+
+    _handleErrorResponse(error: any, taskId: string) {
+        this.taskManager.removeTask(taskId);
+        let errorMessage;
+        let errorTitle;
+        switch (error.status) {
+            case 401:
+                errorMessage = 'No estas autorizado';
+                errorTitle = "Hubo un inconveniente";
+                this.router.navigate(['login']);
+            break;
+            case 404:
+                errorMessage = error.message;
+                errorTitle = "No se encontro el metodo";
+            break;
+            case 500:
+                errorMessage = error.message;
+                errorTitle = "Error Inesperado";
+            break;
+        }
+        
+        //this.toasterService.pop('error', errorTitle, errorMessage);
+    }
+
+    _handleSuccessResponse(response: any, taskId: string) {
+        let resp = response.body;
+        let task = this.taskManager.getTask(taskId);
+        task.loading = false;
+        this.taskManager.updateTask(taskId, task);
+        this.taskManager.removeTask(taskId);
+        
+
+        if(resp.status) {
+            localStorage.setItem('tk', resp.token);
+            return resp;
+        } else {
+            //this.toasterService.pop('error', 'Upss', resp.message);
+        }
+    }
+}
---
+++ src/app/shared/services/task-manager.service.ts
@@ -0,0 +1,47 @@
+﻿import { Injectable } from "@angular/core";
+import { Observable, BehaviorSubject } from "rxjs";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Task } from '../../shared/models/task.model';
+
+@Injectable()
+export class TaskManagerService {
+
+    taskList: Array<Task> = [];
+    private taskChange: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+    public taskChangeEmitter: Observable<boolean> = this.taskChange.asObservable();
+   
+    addTask(task: Task) {
+        const taskIndex = this.taskList.findIndex((existingTask) => existingTask.id === task.id);
+        if (taskIndex === -1) {
+            task.count = 1;
+            this.taskList.push(task);
+        } else {
+            this.taskList[taskIndex].count++;
+        }
+        this.taskChange.next(true);
+    }
+
+    updateTask(id: string, task:Task) {
+        const taskIndex = this.taskList.findIndex((existingTask) => existingTask.id === id);
+        this.taskList[taskIndex] = task;
+        this.taskChange.next(true);
+    }
+
+    removeTask(id: string) {
+        const taskIndex = this.taskList.findIndex((existingTask) => existingTask.id === id);
+        this.taskList[taskIndex].count--;
+        if (this.taskList[taskIndex].count === 0) {
+            this.taskList.splice(taskIndex, 1);
+        }
+        this.taskChange.next(false);
+    }
+
+    getTask(id:string): Task | null {
+        const taskIndex = this.taskList.findIndex((existingTask) => existingTask.id === id);
+        if (taskIndex != -1) {
+            return this.taskList[taskIndex];
+        } else {
+            return null;
+        }
+    }
+}
