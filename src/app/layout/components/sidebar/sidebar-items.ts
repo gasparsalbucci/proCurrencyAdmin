3230ace8c5c410f12eef6cf73a8a77d9cf254395
gasparsalbucci <gasparsalbucci@gmail.com>
2019-04-17 19:12:27 -0300

    Feature/sp1 login (#2)
    * Gaspar Salbucci - login fuctionality
    
    * Gaspar Salbucci - Se agrega items del sibebar mock

---
+++ src/app/layout/components/sidebar/sidebar-items.ts
@@ -0,0 +1,20 @@
+export const  SidebarItems = [
+    {
+        label: 'Dashboard',
+        link: '/dashboard',
+        icon: 'fa fa-fw fa-dashboard',
+        items: []
+    },
+    // {
+    //     label: 'Grid',
+    //     link: '/grid',
+    //     icon: 'fa fa-fw fa-dashboard',
+    //     items: [
+    //         {
+    //             label: 'Dashboard',
+    //             link: '/dashboard',
+    //             icon: 'fa fa-fw fa-dashboard'
+    //         }
+    //     ]
+    // }
+];
