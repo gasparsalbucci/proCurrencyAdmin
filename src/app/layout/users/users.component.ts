258f8bc14be5873203011a3a153e2fc7627703c9
Gaspar Salbucci <gasparsalbucci@gmail.com>
2019-04-23 08:34:54 -0300

    Gaspar Salbucci - users

--- src/app/layout/users/users.component.ts
+++ src/app/layout/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { Users } from './users.model';
 
 @Component({
     selector: 'users',
@@ -7,7 +8,22 @@ import { routerTransition } from '../../router.animations';
     styleUrls: ['./users.component.scss'],
     animations: [routerTransition()]
 })
+
 export class UsersComponent implements OnInit {
+    users : Array<Users> = [
+        {
+            firstName: 'Gaspar',
+            lastName: 'Salbucci',
+            email: 'gaspar@gaspar.com',
+            telephone: '234234234232'
+        },
+        {
+            firstName: 'Gustavo',
+            lastName: 'García',
+            email: 'gustavo@gaspar.com',
+            telephone: '234834236232'
+        }
+    ];
     constructor() {}
 
     ngOnInit() {}
